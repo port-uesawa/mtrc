f917d6a2dc61284ec408bb194488e9f2b8464646
PT1229 <keita.uesawa@theport.jp>
2017-08-26 18:56:31 +0900

    hgf

---
+++ components/OverrayHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav id="overray_header">
+    <i class="header_logo"></i>
+    <ul data-overray-list>
+      <li v-for="item in menu">{{item.content}}</li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        menuContents: [
+          {content: 'TOP'},
+          {content: 'ABOUT'},
+          {content: 'CONTENTS'},
+          {content: 'CONTACT'}
+        ]
+      }
+    },
+    head () {
+      return {
+        meta: [
+          { hid: 'description', name: 'description', content: 'My custom description' }
+        ]
+      }
+    }
+  }
+</script>
+<style>
+</style>
