8da65e934beff3c5e3f8b3643566a71df0c32671
PT1229 <keita.uesawa@theport.jp>
2017-09-27 02:15:45 +0900

    926 add

---
+++ components/game/CharactorArea.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class='l-charactor-area'></div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        menuContents: [
+          {content: 'TOP'},
+          {content: 'ABOUT'},
+          {content: 'CONTENTS'},
+          {content: 'CONTACT'}
+        ]
+      }
+    },
+    head () {
+      return {
+        meta: [
+          { hid: 'description', name: 'description', content: 'My custom description' }
+        ],
+        link: [
+          // { rel: 'stylesheet', href: '/assets/stylesheets/components/common/overlay_header.scss', lang: 'scss' }
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .l-charactor-area {
+    position: absolute;
+    z-index: 1;
+    width:100%;
+    height:100%;
+  }
+</style>
