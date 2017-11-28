7544951e3a7773721e66b4ba7bfdaf6f4bd04b24
PT1229 <keita.uesawa@theport.jp>
2017-11-28 12:31:17 +0900

    postmessage

---
+++ pages/postMessageTest.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <section class="l-main">
+    <p>test</p>
+    
+  </section>
+</template>
+
+<script>
+  export default {
+    head: {
+    },
+    mounted () {
+      parent.postMessage('fugaaaaaaa', 'https://jsfiddle.net/')
+    }
+  }
+</script>
+
+<style lang="scss">
+    p{
+        color: white
+    }
+</style>
