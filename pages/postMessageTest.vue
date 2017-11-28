768f926e9e5c07a4904bb00a813c642cf29999d2
PT1229 <keita.uesawa@theport.jp>
2017-11-28 12:49:29 +0900

    url shuusei

--- pages/postMessageTest.vue
+++ pages/postMessageTest.vue
@@ -2,16 +2,16 @@
 
   <section class="l-main">
     <p>test</p>
-    
+    <input type='button' value='yahooにジャンプ' v-on:click="this.postMsg" />
   </section>
 </template>
 
 <script>
   export default {
-    head: {
-    },
-    mounted () {
-      parent.postMessage('fugaaaaaaa', 'https://fiddle.jshell.net/')
+    methods: {
+      postMsg () {
+        parent.postMessage('https://www.yahoo.co.jp/', 'https://fiddle.jshell.net/')
+      }
     }
   }
 </script>
